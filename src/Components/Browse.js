f033c7bd302672f67e2af02072dfd4411a34de0e
OmKeshri <omkeshri21@gmail.com>
2024-08-19 18:01:58 +0530

    api fetch added browse page main container

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,27 +1,16 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../Utils/constants";
+import useNowPlayingMovies from "../Hooks/useNowPlayingMovies";
 import Header from "./Header";
-import { useDispatch } from "react-redux";
-import { addNowPlayingMovies } from "../Utils/movieSlice";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-    dispatch(addNowPlayingMovies(json.results))
-  };
+  useNowPlayingMovies();
 
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, [])
   return (
     <div>
       <Header />
+      <MainContainer/>
+      <SecondaryContainer/>
     </div>
   );
 };
